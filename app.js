4472577a169c626353a60106da131ce061817b56
Domdae <phonmongkhon_ph@cmu.ac.th>
2023-03-18 17:01:21 +0700

    [basic]: project 01

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-let n = prompt("name")
+let n = prompt("เลขหวย")
 console.log(n);
-console.log(typeof n);
+// Math.floor(Math.random() * 10); // 0-9
+// Math.floor(Math.random() * 11); // 0-10
+document.getElementById("number").innerHTML = n;
+document.getElementById("result").innerHTML = Math.floor(Math.random() * 100);
