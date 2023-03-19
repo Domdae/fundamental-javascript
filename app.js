5ee1584ccc6e072fb851d20a4341ceba034e3c0d
Domdae <domdae.ph@gmail.com>
2023-03-19 16:49:34 +0700

    [condition]: if else condition

---
+++ app.js
@@ -0,0 +1,5 @@
+let age = prompt("please input your old:");
+if (age > 13) {
+    document.getElementById("content").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/sVTy_wmn5SU\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
+}
+// when we need to use " in text using \ before "
