3414a56af9b4b8b7048e70685b065d80d945e62c
Domdae <domdae.ph@gmail.com>
2023-03-19 17:02:15 +0700

    [condition]: project 02

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ console.log(n);
 // Math.floor(Math.random() * 10); // 0-9
 // Math.floor(Math.random() * 11); // 0-10
 document.getElementById("number").innerHTML = n;
-document.getElementById("result").innerHTML = Math.floor(Math.random() * 100);
+let random = Math.floor(Math.random() * 100);
+document.getElementById("random").innerHTML = random;
+if (n === random) {
+    document.getElementById("result").innerHTML = "congrets !!!!";
+
+} else {
+    document.getElementById("result").innerHTML = "wwkekkrrkrkrkrkrkrk !!!!";
+}
